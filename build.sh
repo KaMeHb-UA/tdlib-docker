51626d91bf43905017ac1cbf9bc5e007beabc984
KaMeHb-UA <marlock@bassteam.ml>
2021-02-10 09:59:35 +0200

    add support for more platforms

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 PLATFORMS="linux/386,linux/amd64,linux/arm/v5,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/mips64le,linux/s390x"
 DH_NAME="kamehb/tdlib"
 
-docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 docker buildx use stdbuilder || docker buildx create --use --name stdbuilder
 
